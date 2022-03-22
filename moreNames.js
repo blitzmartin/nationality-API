6b7657ae710f02d7a7f8dca54d154adea3063305
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-03-22 15:07:24 +0100

    script takes more names and put them in an array

---
+++ moreNames.js
@@ -0,0 +1,40 @@
+
+        const btn = document.getElementById("button");
+        const input = document.querySelector("#nameInput");
+        btn.addEventListener("click", newFunction)
+
+        function newFunction(event) {
+
+            let nameValue = this.previousElementSibling.value;
+            console.log("You are searching for the name " + nameValue);
+            const URL = `https://api.nationalize.io/?name=${nameValue}`;
+            var tempArray = []
+            tempArray = nameValue.split(",");
+            console.log(tempArray);
+
+            fetch(URL)
+                .then(function (response) {
+                    if (response.ok) {
+                        return response.json()
+                    }
+                    return Promise.reject('something went wrong')
+                })
+                .then(function (data) {
+
+                    const returnName = data.name;
+                    document.getElementById("solutionName").innerHTML = returnName.toUpperCase() + " has got:";
+                    const countryArray = [];
+                    const probArray = [];
+                    for (let i = 0; i < 3; i++) {
+                        countryArray.push(data.country[i].country_id);
+                        probArray.push(data.country[i].probability.toFixed(3));
+                        document.getElementById("solutionInfo").innerHTML += probArray[i] + " probability to be in " + countryArray[i] + ", ";
+                    }
+
+
+
+                })
+                .catch(function (error) {
+                    console.log("error is", error)
+                })
+        }
