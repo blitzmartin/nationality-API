fd6d42bb1ebe400ce0e3dd30f69e4313c625f388
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-03-22 15:46:52 +0100

    last commit perchè mi soccio

--- oneName.js
+++ oneName.js
@@ -1,34 +1,39 @@
 
-        const btn = document.getElementById("button");
-        const input = document.querySelector("#nameInput");
-        btn.addEventListener("click", newFunction)
+const btn = document.getElementById("button");
+const input = document.querySelector("#nameInput");
+btn.addEventListener("click", newFunction)
 
-        function newFunction(event) {
+function newFunction(event) {
 
-            let nameValue = this.previousElementSibling.value;
-            const URL = `https://api.nationalize.io/?name=${nameValue}`;
-     
+    let nameValue = this.previousElementSibling.value;
+    const URL = `https://api.nationalize.io/?name=${nameValue}`;
+    var namesArray = [];
+    namesArray = nameValue.split(",");
+    console.log(namesArray);
 
-            fetch(URL)
-                .then(function (response) {
-                    if (response.ok) {
-                        return response.json()
-                    }
-                    return Promise.reject('something went wrong')
-                })
-                .then(function (data) {
 
-                    const returnName = data.name;
-                    document.getElementById("solutionName").innerHTML = returnName.toUpperCase() + " has got:";
-                    const countryArray = [];
-                    const probArray = [];
-                    for (let i = 0; i < 3; i++) {
-                        countryArray.push(data.country[i].country_id);
-                        probArray.push(data.country[i].probability.toFixed(3));
-                        document.getElementById("solutionInfo").innerHTML += probArray[i] + " probability to be in " + countryArray[i] + ", ";
-                    }
-                })
-                .catch(function (error) {
-                    console.log("error is", error)
-                })
-        }
+    fetch(URL)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json()
+            }
+            return Promise.reject('something went wrong')
+        })
+        .then(function (data) {
+
+            const returnName = data.name;
+
+            document.getElementById("solutionName").innerHTML = returnName.toUpperCase() + " has got:";
+            const countryArray = [];
+            const probArray = [];
+            for (let i = 0; i < 3; i++) {
+                countryArray.push(data.country[i].country_id);
+                probArray.push(data.country[i].probability.toFixed(3));
+                document.getElementById("solutionInfo").innerHTML += probArray[i] + " probability to be in " + countryArray[i] + ", ";
+            }
+
+        })
+        .catch(function (error) {
+            console.log("error is", error)
+    })
+}
